6b72180dc6b2e521f87c7dad8e2c6e9e6c3db55b
Unknown <neudor@gmail.com>
2020-11-17 14:01:50 +0500

    feat: basic nova field

---
+++ src/Unsplash.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Shed\Unsplash;
+
+use Laravel\Nova\Fields\Field;
+
+class Unsplash extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'unsplash';
+}
